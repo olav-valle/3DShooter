3135013e3e9b123332992cdcaf170c480b35e0b5
Olav Valle <olavval@stud.ntnu.no>
2021-10-09 21:25:44 +0200

    finish "Writing The Player Control Script 2"

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -7,11 +7,28 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
-    public Shooter playerShooter;
+    [Header("Settings")] [Tooltip("The speed at which the player moves")]
     public float moveSpeed = 2f;
+
+    [Tooltip("The speed at which the player rotates to look left and right (calculated in degrees)")]
     public float lookSpeed = 60f;
+
+    [Tooltip("The power that the player jumps with")]
     public float jumpPower = 8f;
+
+    [Tooltip("The force of gravity acting on the player")]
     public float gravity = 9.81f;
+
+    [Header("Required references")] [Tooltip("The player shooter script that fires projectiles")]
+    public Shooter playerShooter;
+
+
+    // Additional components of the player component.
+    private CharacterController controller;
+    private InputManager inputManager;
+
+
+
 
     /// <summary>
     /// Description:
@@ -23,8 +40,31 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Start()
     {
-
+        SetUpCharacterController();
+        SetUpInputManager();
     }
+
+    /// <summary>
+    /// Set up CharacterController object for this object.
+    /// </summary>
+    private void SetUpCharacterController()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+        {
+            Debug.LogError("The player controller script does not have a CharacterController on the same object.");
+        }
+    }
+
+    private void SetUpInputManager()
+    {
+        inputManager = GetComponent<InputManager>();
+        if (inputManager == null)
+        {
+            Debug.LogError("The player controller script does not have an InputManager on the same object.");
+        }
+    }
+
 
     /// <summary>
     /// Description:
